e24f3359dbe41f741172992ece81812f8329584f
zxw <xingwei.zhang@outlook.com>
2019-01-25 16:02:07 +0800

    Modify ajshop sql file

--- AJShop/build/classes/ajshop.sql
+++ AJShop/build/classes/ajshop.sql
@@ -29,7 +29,7 @@ CREATE TABLE `adminuser` (
 -- ----------------------------
 -- Records of adminuser
 -- ----------------------------
-INSERT INTO `adminuser` VALUES ('2', 'admin', 'admin');
+INSERT INTO `adminuser` VALUES ('1', 'admin', 'admin');
 
 -- ----------------------------
 -- Table structure for category
@@ -44,12 +44,12 @@ CREATE TABLE `category` (
 -- ----------------------------
 -- Records of category
 -- ----------------------------
-INSERT INTO `category` VALUES ('1', '水果基地');
-INSERT INTO `category` VALUES ('2', '安全频道');
-INSERT INTO `category` VALUES ('3', '商城卡');
-INSERT INTO `category` VALUES ('4', '定制套餐');
-INSERT INTO `category` VALUES ('5', '节气养生');
-INSERT INTO `category` VALUES ('6', '便民服务');
+INSERT INTO `category` VALUES ('1', '首页');
+INSERT INTO `category` VALUES ('2', '热门分类');
+INSERT INTO `category` VALUES ('3', '专属定制');
+INSERT INTO `category` VALUES ('4', '商城卡');
+INSERT INTO `category` VALUES ('5', '会员专区');
+INSERT INTO `category` VALUES ('6', '联系方式');
 
 -- ----------------------------
 -- Table structure for categorysecond
@@ -67,19 +67,12 @@ CREATE TABLE `categorysecond` (
 -- ----------------------------
 -- Records of categorysecond
 -- ----------------------------
-INSERT INTO `categorysecond` VALUES ('1', '西瓜', '1');
-INSERT INTO `categorysecond` VALUES ('2', '樱桃', '1');
-INSERT INTO `categorysecond` VALUES ('37', '百香果', '1');
-INSERT INTO `categorysecond` VALUES ('38', '菠萝', '1');
-INSERT INTO `categorysecond` VALUES ('39', '草莓', '1');
-INSERT INTO `categorysecond` VALUES ('40', '橙子', '1');
-INSERT INTO `categorysecond` VALUES ('41', '脆枣', '1');
-INSERT INTO `categorysecond` VALUES ('42', '哈密瓜', '1');
-INSERT INTO `categorysecond` VALUES ('43', '金橘', '1');
-INSERT INTO `categorysecond` VALUES ('44', '榴莲', '1');
-INSERT INTO `categorysecond` VALUES ('45', '毛桃', '1');
-INSERT INTO `categorysecond` VALUES ('46', '牛油果', '1');
-INSERT INTO `categorysecond` VALUES ('47', '柠檬', '1');
+INSERT INTO `categorysecond` VALUES ('1', 'AJ1', '2');
+INSERT INTO `categorysecond` VALUES ('5', 'AJ5', '2');
+INSERT INTO `categorysecond` VALUES ('11', 'AJ11', '2');
+INSERT INTO `categorysecond` VALUES ('12', 'AJ12', '2');
+INSERT INTO `categorysecond` VALUES ('13', 'AJ13', '2');
+
 
 -- ----------------------------
 -- Table structure for orderitem
@@ -102,11 +95,6 @@ CREATE TABLE `orderitem` (
 -- ----------------------------
 -- Records of orderitem
 -- ----------------------------
-INSERT INTO `orderitem` VALUES ('18', '1', '18', '53', '9023');
-INSERT INTO `orderitem` VALUES ('19', '1', '4', '21', '9024');
-INSERT INTO `orderitem` VALUES ('20', '1', '3', '51', '9025');
-INSERT INTO `orderitem` VALUES ('21', '1', '15', '35', '9026');
-INSERT INTO `orderitem` VALUES ('22', '1', '10', '1', '9026');
 
 -- ----------------------------
 -- Table structure for orders
@@ -129,20 +117,6 @@ CREATE TABLE `orders` (
 -- ----------------------------
 -- Records of orders
 -- ----------------------------
-INSERT INTO `orders` VALUES ('9013', '25', '2018-11-16 00:00:00', '2', 'qwe', '15640444444', '123', '10');
-INSERT INTO `orders` VALUES ('9014', '20', '2018-11-16 00:00:00', '2', 'qwe', '15640444444', '123', '10');
-INSERT INTO `orders` VALUES ('9015', '10', '2018-11-16 00:00:00', '2', 'qwe', '15640444444', '123', '10');
-INSERT INTO `orders` VALUES ('9016', '10', '2018-11-16 00:00:00', '1', null, null, null, '10');
-INSERT INTO `orders` VALUES ('9017', '14', '2018-11-16 00:00:00', '1', null, null, null, '10');
-INSERT INTO `orders` VALUES ('9018', '17', '2018-11-16 00:00:00', '1', null, null, null, '10');
-INSERT INTO `orders` VALUES ('9019', '18', '2018-11-16 00:00:00', '1', null, null, null, '10');
-INSERT INTO `orders` VALUES ('9020', '4', '2018-11-16 00:00:00', '1', null, null, null, '10');
-INSERT INTO `orders` VALUES ('9021', '18', '2018-11-16 00:00:00', '1', null, null, null, '10');
-INSERT INTO `orders` VALUES ('9022', '18', '2018-11-16 00:00:00', '1', null, null, null, '10');
-INSERT INTO `orders` VALUES ('9023', '18', '2018-11-16 00:00:00', '3', 'qwe', '15640444444', '123', '10');
-INSERT INTO `orders` VALUES ('9024', '4', '2018-11-17 00:00:00', '3', 'qwe', '15640444444', '123', '10');
-INSERT INTO `orders` VALUES ('9025', '3', '2018-11-18 00:00:00', '3', 'sxk', '15612341234', '辽宁省大连市', '7');
-INSERT INTO `orders` VALUES ('9026', '25', '2018-11-19 00:00:00', '3', 'sxk', '15726607618', '辽宁省大连市', '7');
 
 -- ----------------------------
 -- Table structure for product
@@ -166,16 +140,8 @@ CREATE TABLE `product` (
 -- ----------------------------
 -- Records of product
 -- ----------------------------
-INSERT INTO `product` VALUES ('1', '百香果', '15', '10', 'products/baixiangguo.jpg', '限量一斤', '1', '2014-11-02 20:18:00', '37');
-INSERT INTO `product` VALUES ('21', '菠萝', '5', '4', 'products/boluo.jpg', '限量一斤', '1', '2014-11-03 20:18:00', '38');
-INSERT INTO `product` VALUES ('35', '草莓', '20', '15', 'products/caomei.jpg', '限量一斤', '1', '2014-11-03 20:18:00', '39');
-INSERT INTO `product` VALUES ('41', '橙子', '5', '3', 'products/chengzi.jpg', '限量一斤', '1', '2014-11-03 20:18:00', '40');
-INSERT INTO `product` VALUES ('42', '脆枣', '12', '10', 'products/cuizao.jpg', '限量一斤', '1', '2014-11-03 20:18:00', '41');
-INSERT INTO `product` VALUES ('44', '哈密瓜', '4', '3', 'products/hamigua.jpg', '限量一斤', '1', '2014-11-03 20:18:00', '42');
-INSERT INTO `product` VALUES ('47', '金橘', '2', '1', 'products/jinju.jpg', '限量一斤', '1', '2014-11-03 20:18:00', '43');
-INSERT INTO `product` VALUES ('51', '西瓜', '5', '3', 'products/xigua.jpg', '限量一斤', '1', '2014-12-07 20:18:00', '1');
-INSERT INTO `product` VALUES ('53', '榴莲', '20', '18', 'products/liulian.jpg', '限量一斤', '1', '2014-11-03 20:18:00', '44');
-INSERT INTO `product` VALUES ('57', '毛桃', '4', '3', 'products/maotao.jpg', '限量一斤', '1', '2018-11-19 00:00:00', '45');
+INSERT INTO `product` VALUES ('1', '', '15', '10', 'products/baixiangguo.jpg', '限量一斤', '1', '2014-11-02 20:18:00', '37');
+
 
 -- ----------------------------
 -- Table structure for user
@@ -197,5 +163,5 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('7', 'sxk', '123', 'sxk', 'aaa@shop.com', '15726607618', '辽宁省大连市', null, null);
-INSERT INTO `user` VALUES ('10', '123', '123', 'qwe', '353201814@qq.com', '15640444444', '123', null, null);
+INSERT INTO `user` VALUES ('1', 'ahh', '123', 'ahh', 'ahh@shop.com', '15640111111', '英国', null, null);
+INSERT INTO `user` VALUES ('2', '123', '123', '123', '123@shop.com', '15640444444', '辽宁省大连市', null, null);
